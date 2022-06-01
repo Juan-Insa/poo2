615d86d4f7a4ba724f4282addcc57cd34f13cf21
Juan Cruz Insaurralde <70179741+Juan-Insa@users.noreply.github.com>
2022-06-01 19:44:14 -0300

    Update Ejercicio4.cpp

--- EstrD - Practicas - Resoluciones/Practica 9/Ejercicio4.cpp
+++ EstrD - Practicas - Resoluciones/Practica 9/Ejercicio4.cpp
@@ -24,6 +24,7 @@ void cuentaRegresiva(int n){
 }
 
 // Propósito: imprime los números desde n hasta 0, separados por saltos de línea.
+// precondición: n es positivo.
 // utiliza recursión.
 void cuentaRegresivaRec(int n){
     if (n>0){
@@ -35,7 +36,7 @@ void cuentaRegresivaRec(int n){
 // Propósito: imprime los números de 0 hasta n, separados por saltos de línea.
 // utiliza iteración.
 void desdeCeroHastaN(int n){
-    for(i=0; i<=n; i++){
+    for(int i=0; i<=n; i++){
         cout << actual << endl;
         actual++;
     }
@@ -70,6 +71,77 @@ int multRec(int n, int m){
         return 0;
     }    
 }
+
+// iteración
+void primerosN(int n, string s){
+    for(int i=0; i > n; i++){
+        cout << s[i] << endl;        
+    }
+}
+
+// recursión
+void primerosNRec(int n, string s){
+    return caracteresHasta(n,s,0);
+}
+
+void caracteresHasta(int n, string s, int m){
+  if (n<=m){
+         cout << s[m] << endl;   
+         primerosNRec(n, s, m++);
+  }
+}
+
+// iteración
+bool pertenece(char c, string s){
+    int i = 0;
+    while(s[i] != null){
+        i++;
+    }
+    return c == s[i];
+}
+
+// recursión
+bool perteneceRec(char c, string s){
+    return belongsRec(c, s, 0);
+}
+
+bool belongsRec(char c, string s, int n){
+    if(s[n] == null){
+        return s[n] == c;
+    } else {
+        return s[n] == c || belongsRec(c, s, n++);
+    }     
+}
+
+// iterativo
+int apariciones(char c, string s){
+    int apariciones = 0;
+    for(int i=0; s[i] != null; i++){
+        apariciones += unoSi(s[i] == c);
+    }
+}
+
+int unoSi(bool cond){
+       if (cond){
+           return 1;
+       } else {
+           return 0;
+       }
+}
+
+// recursivo
+int aparicionesRec(char c, string s){
+    return aparicionesDe(c,s,0);
+}
+
+int aparicionesDe(char c, string s, int n){
+    if(s[n] == null){
+        return unoSi(s[i] == c);
+    } else {
+        return unoSi(s[i] == c) + aparicionesDe(c,s,n++);
+    }
+}
+
 
 
 
